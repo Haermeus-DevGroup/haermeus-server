ad4ddaeb2c417429dbf3ded27cd608847bc755ff
midavuha1 <106444335+midavuha1@users.noreply.github.com>
2022-12-18 16:19:06 +0300

    Добавил предварительные javax.persistence аннотации на сущности

--- src/main/java/dev/haermeus/haermeusserver/entity/FileReferenceEntity.java
+++ src/main/java/dev/haermeus/haermeusserver/entity/FileReferenceEntity.java
@@ -1,15 +1,23 @@
 package dev.haermeus.haermeusserver.entity;
 import lombok.*;
 
+import javax.persistence.*;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode
 @ToString
+@Entity
+@Table(name = "file_reference")
 public class FileReferenceEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
     @ToString.Exclude @EqualsAndHashCode.Exclude private ResourceEntity resource;
+    @Column(name = "relative_path")
     private String relativePath;
 
 }
