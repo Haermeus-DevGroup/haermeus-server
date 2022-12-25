4d7b68f75549db586833776eab8f2336717d6b1a
midavuha1 <106444335+midavuha1@users.noreply.github.com>
2022-12-25 21:51:09 +0300

    создал базу сервиса для получения секций

---
+++ src/main/java/dev/haermeus/haermeusserver/service/SectionService.java
@@ -0,0 +1,16 @@
+package dev.haermeus.haermeusserver.service;
+
+import dev.haermeus.haermeusserver.dto.resource.PlainResourceDTO;
+import dev.haermeus.haermeusserver.dto.section.PlainSectionDTO;
+
+import java.util.List;
+
+public interface SectionService {
+
+    PlainSectionDTO getPlainSection(Long id);
+    List<PlainSectionDTO> getPlainChildrenSections(Long id);
+    List<PlainResourceDTO> getPlainChildrenResources(Long id);
+
+
+
+}
